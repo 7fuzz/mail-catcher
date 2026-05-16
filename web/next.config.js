3254d8c97e7b0108294921b8872ebde1ffef6053
Fauzi <f@uzi.my.id>
2026-05-16 14:47:48 +0700

    Fix: Remove conflicting transpilePackages for @prisma/client

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@prisma/client'],
-  // Fix for multiple lockfiles warning and path resolution
-  transpilePackages: ['@prisma/client'],
 }
 
 export default nextConfig
